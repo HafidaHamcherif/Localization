99f1dd496573fcefe3adcbeadbcf08e6c94a3788
HafidaHamcherif <58478522+HafidaHamcherif@users.noreply.github.com>
2020-08-21 16:45:42 +0200

    Localization

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const exphbs = require('express-handlebars') ;
+const port = 3000;
+
+const app = express()
+//const id = 'abc'
+
+
+app.use(express.static('public'));
+app.engine('handlebars',exphbs({defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/',(req,res)=>{
+    res.render('home');
+})
+
+app.get('/lang',(req,res)=>{
+    res.render('lang',{
+        france:'France',
+        en:'English',
+        es:'Espanol',
+        //lang: req.params.lang,
+        //id
+    });
+})
+
+app.get('/france',(req,res)=>{
+    res.render('france',{
+        pageTitle: "Bonjour",
+        title: "ça va ?"
+    });
+})
+
+app.get('/English',(req,res)=>{
+    res.render('English',{
+        pageTitle: "Hello",
+        title: "ça va ?"
+    });
+})
+
+app.get('/Espanol',(req,res)=>{
+    res.render('Espanol',{
+        pageTitle: "Hola",
+        title: "ça va ?"
+    });
+})
+
+
+app.listen(port, ()=>{
+    console.log(`start serveur : ${port}`);
+})
